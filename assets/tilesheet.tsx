0cbb91ab12cb9a29d50171d447d50b40d3c8f95e
Andor Saga <andor.saga@gmail.com>
2020-05-19 18:25:06 -0400

    adding map and tileset.

---
+++ assets/tilesheet.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="tilesheet" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <editorsettings>
+  <export target="tilesheet.json" format="json"/>
+ </editorsettings>
+ <image source="tilesheet.png" width="320" height="320"/>
+ <tile id="0">
+  <properties>
+   <property name="dummy" type="bool" value="false"/>
+   <property name="wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="dummy" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="dummy" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="dummy" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="dummy" type="bool" value="false"/>
+   <property name="wall" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
